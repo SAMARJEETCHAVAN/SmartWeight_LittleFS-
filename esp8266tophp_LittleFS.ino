4a7355fde7526f8de7a1f46c136fc32accbf7d75
SAMARJEETCHAVAN <75219762+SAMARJEETCHAVAN@users.noreply.github.com>
2021-10-02 16:54:40 +0530

    Update esp8266tophp_LittleFS.ino

--- esp8266tophp_LittleFS.ino
+++ esp8266tophp_LittleFS.ino
@@ -9,9 +9,9 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
-#include <Arduino.h>
-#include "FS.h"
-#include <LITTLEFS.h>
+#include <FS.h>
+#include <LittleFS.h>
+#include <time.h>
 
 #define LED LED_BUILTIN // onboard LED
 
@@ -31,238 +31,88 @@ const char *ssid = "FENET";  //ENTER YOUR WIFI SETTINGS
 const char *password = "12345678";
 String ServerAddrStr = "http://192.168.2.101/wtdata/";
 #endif
+void listDir(const char * dirname) {
+  Serial.printf("Listing directory: %s\n", dirname);
 
+  Dir root = LittleFS.openDir(dirname);
+
+  while (root.next()) {
+    File file = root.openFile("r");
+    Serial.print("  FILE: ");
+    Serial.print(root.fileName());
+    Serial.print("  SIZE: ");
+    Serial.print(file.size());
+    time_t cr = file.getCreationTime();
+    time_t lw = file.getLastWrite();
+    file.close();
+    struct tm * tmstruct = localtime(&cr);
+    Serial.printf("    CREATION: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+    tmstruct = localtime(&lw);
+    Serial.printf("  LAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+  }
+}
+
+
+void readFile(const char * path) {
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = LittleFS.open(path, "r");
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while (file.available()) {
+    Serial.write(file.read());
+  }
+  file.close();
+}
+
+void writeFile(const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = LittleFS.open(path, "w");
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  delay(2000); // Make sure the CREATE and LASTWRITE times are different
+  file.close();
+}
+
+void appendFile(const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = LittleFS.open(path, "a");
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+void renameFile(const char * path1, const char * path2) {
+  Serial.printf("Renaming file %s to %s\n", path1, path2);
+  if (LittleFS.rename(path1, path2)) {
+    Serial.println("File renamed");
+  } else {
+    Serial.println("Rename failed");
+  }
+}
 //=======================================================================
 //                    Power on setup
 //=======================================================================
-#define FORMAT_LITTLEFS_IF_FAILED true
-
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-    Serial.printf("Listing directory: %s\r\n", dirname);
-
-    File root = fs.open(dirname);
-    if(!root){
-        Serial.println("- failed to open directory");
-        return;
-    }
-    if(!root.isDirectory()){
-        Serial.println(" - not a directory");
-        return;
-    }
-
-    File file = root.openNextFile();
-    while(file){
-        if(file.isDirectory()){
-            Serial.print("  DIR : ");
-            Serial.println(file.name());
-            if(levels){
-                listDir(fs, file.name(), levels -1);
-            }
-        } else {
-            Serial.print("  FILE: ");
-            Serial.print(file.name());
-            Serial.print("\tSIZE: ");
-            Serial.println(file.size());
-        }
-        file = root.openNextFile();
-    }
-}
-
-void createDir(fs::FS &fs, const char * path){
-    Serial.printf("Creating Dir: %s\n", path);
-    if(fs.mkdir(path)){
-        Serial.println("Dir created");
-    } else {
-        Serial.println("mkdir failed");
-    }
-}
-
-void removeDir(fs::FS &fs, const char * path){
-    Serial.printf("Removing Dir: %s\n", path);
-    if(fs.rmdir(path)){
-        Serial.println("Dir removed");
-    } else {
-        Serial.println("rmdir failed");
-    }
-}
-
-void readFile(fs::FS &fs, const char * path){
-    Serial.printf("Reading file: %s\r\n", path);
-
-    File file = fs.open(path);
-    if(!file || file.isDirectory()){
-        Serial.println("- failed to open file for reading");
-        return;
-    }
-
-    Serial.println("- read from file:");
-    while(file.available()){
-        Serial.write(file.read());
-    }
-    file.close();
-}
-
-void writeFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Writing file: %s\r\n", path);
-
-    File file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("- failed to open file for writing");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("- file written");
-    } else {
-        Serial.println("- write failed");
-    }
-    file.close();
-}
-
-void appendFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Appending to file: %s\r\n", path);
-
-    File file = fs.open(path, FILE_APPEND);
-    if(!file){
-        Serial.println("- failed to open file for appending");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("- message appended");
-    } else {
-        Serial.println("- append failed");
-    }
-    file.close();
-}
-
-void renameFile(fs::FS &fs, const char * path1, const char * path2){
-    Serial.printf("Renaming file %s to %s\r\n", path1, path2);
-    if (fs.rename(path1, path2)) {
-        Serial.println("- file renamed");
-    } else {
-        Serial.println("- rename failed");
-    }
-}
-
-void deleteFile(fs::FS &fs, const char * path){
-    Serial.printf("Deleting file: %s\r\n", path);
-    if(fs.remove(path)){
-        Serial.println("- file deleted");
-    } else {
-        Serial.println("- delete failed");
-    }
-}
-
-// SPIFFS-like write and delete file, better use #define CONFIG_LITTLEFS_SPIFFS_COMPAT 1
-
-void writeFile2(fs::FS &fs, const char * path, const char * message){
-    if(!fs.exists(path)){
-    if (strchr(path, '/')) {
-            Serial.printf("Create missing folders of: %s\r\n", path);
-      char *pathStr = strdup(path);
-      if (pathStr) {
-        char *ptr = strchr(pathStr, '/');
-        while (ptr) {
-          *ptr = 0;
-          fs.mkdir(pathStr);
-          *ptr = '/';
-          ptr = strchr(ptr+1, '/');
-        }
-      }
-      free(pathStr);
-    }
-    }
-
-    Serial.printf("Writing file to: %s\r\n", path);
-    File file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("- failed to open file for writing");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("- file written");
-    } else {
-        Serial.println("- write failed");
-    }
-    file.close();
-}
-
-void deleteFile2(fs::FS &fs, const char * path){
-    Serial.printf("Deleting file and empty folders on path: %s\r\n", path);
-
-    if(fs.remove(path)){
-        Serial.println("- file deleted");
-    } else {
-        Serial.println("- delete failed");
-    }
-
-    char *pathStr = strdup(path);
-    if (pathStr) {
-        char *ptr = strrchr(pathStr, '/');
-        if (ptr) {
-            Serial.printf("Removing all empty folders on path: %s\r\n", path);
-        }
-        while (ptr) {
-            *ptr = 0;
-            fs.rmdir(pathStr);
-            ptr = strrchr(pathStr, '/');
-        }
-        free(pathStr);
-    }
-}
-
-void testFileIO(fs::FS &fs, const char * path){
-    Serial.printf("Testing file I/O with %s\r\n", path);
-
-    static uint8_t buf[512];
-    size_t len = 0;
-    File file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("- failed to open file for writing");
-        return;
-    }
-
-    size_t i;
-    Serial.print("- writing" );
-    uint32_t start = millis();
-    for(i=0; i<2048; i++){
-        if ((i & 0x001F) == 0x001F){
-          Serial.print(".");
-        }
-        file.write(buf, 512);
-    }
-    Serial.println("");
-    uint32_t end = millis() - start;
-    Serial.printf(" - %u bytes written in %u ms\r\n", 2048 * 512, end);
-    file.close();
-
-    file = fs.open(path);
-    start = millis();
-    end = start;
-    i = 0;
-    if(file && !file.isDirectory()){
-        len = file.size();
-        size_t flen = len;
-        start = millis();
-        Serial.print("- reading" );
-        while(len){
-            size_t toRead = len;
-            if(toRead > 512){
-                toRead = 512;
-            }
-            file.read(buf, toRead);
-            if ((i++ & 0x001F) == 0x001F){
-              Serial.print(".");
-            }
-            len -= toRead;
-        }
-        Serial.println("");
-        end = millis() - start;
-        Serial.printf("- %u bytes read in %u ms\r\n", flen, end);
-        file.close();
-    } else {
-        Serial.println("- failed to open file for reading");
-    }
-}
 
 void setup() {
 
@@ -298,32 +148,23 @@ void setup() {
     digitalWrite(LED, HIGH); // turn the LED on.
     delay(100);            // wait for 1 second.
   }
-t0 = millis();
-#ifdef TWOPART
-    if(!LITTLEFS.begin(FORMAT_LITTLEFS_IF_FAILED, "/lfs2", 5, "part2")){
-    Serial.println("part2 Mount Failed");
-    return;
-    }
-    appendFile(LITTLEFS, "/hello0.txt", "World0!\r\n");
-    readFile(LITTLEFS, "/hello0.txt");
-    LITTLEFS.end();
+  
 
-    Serial.println( "Done with part2, work with the first lfs partition..." );
-#endif
-
-    if(!LITTLEFS.begin(FORMAT_LITTLEFS_IF_FAILED)){
-        Serial.println("LITTLEFS Mount Failed");
-        return;
-    }
-    createDir(LITTLEFS, "/datadir");
-    writeFile(LITTLEFS, "/datadir/swdata.txt", "START_POINT");
+  writeFile("/swdata.txt", "start");
+  
 }
 
 //=======================================================================
 //                    Main Program Loop
 //=======================================================================
-
-
+int t0 = millis();
+  Serial.println("Formatting LittleFS filesystem");
+  LittleFS.format();
+  Serial.println("Mount LittleFS");
+  if (!LittleFS.begin()) {
+    Serial.println("LittleFS mount failed");
+    return;
+  }
 void loop() {
   char c;
   HTTPClient http;    //Declare object of class HTTPClient
@@ -339,18 +180,18 @@ void loop() {
   {
     case 'w':
       {
-        
           String Str = Serial.readString();
+          // weight, userID, stationID (all separated by comma, ending by $)
+          //postData = "data=" + Str ; 
           if ((t0-millis())<300000){
           postData = Str + "\n" ;
-          appendFile(LITTLEFS, "/datadir/swdata.txt", postData);
+          appendFile(LITTLEFS, "/swdata.txt", postData);
           }
           else if((t0-millis())>=300000){
-            str= readFile(LITTLEFS, "/datadir/swdata.txt");
+             str= readFile(LITTLEFS, "/swdata.txt");
             char * pch;
             pch = strtok (str,"\n");
-            while (pch != "START_POINT"){
-              
+            while (pch != "start"){
 
 //          HttpAddrStr =  ServerAddrStr + "post-data.php";
 
@@ -364,7 +205,7 @@ void loop() {
 
  //         http.begin(HttpAddrStr);              //Specify request destination
           http.addHeader("Content-Type", "application/x-www-form-urlencoded");    //Specify content-type header
-          int httpCode = http.POST(pch);   //Send the request
+          int httpCode = http.POST(postData);   //Send the request
           String payload = http.getString();    //Get the response payload
 
           Serial.println(payload);    //Print request response payload
@@ -378,13 +219,12 @@ void loop() {
           delay(100);            // wait for 1 second.
           digitalWrite(LED, HIGH); // turn the LED on.
         */
-      
-      //printf ("%s\n",pch);
-      pch = strtok (NULL, "\n");
-      }
-      t0 = millis();
-      } 
-      }break; // wi-fi command
+            }
+            pch = strtok (NULL, "\n");
+            }
+            t0 = millis();
+            } break; // wi-fi command
+
     //////////////////////////////////
     // get username from RFID code
 
